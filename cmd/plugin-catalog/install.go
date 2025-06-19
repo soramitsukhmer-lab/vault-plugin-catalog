e849b84c154d6460016070607aa1539b201453da
Socheat Sok <socheatsok78@gmail.com>
2025-06-19 16:31:42 +0700

    Draft a skeleton `install` sub-command

--- cmd/plugin-catalog/install.go
+++ cmd/plugin-catalog/install.go
@@ -2,10 +2,21 @@ package main
 
 import "github.com/urfave/cli/v2"
 
+const defaultVaultPluginsDir = "/vault/plugins"
+
 func installCommand() *cli.Command {
 	return &cli.Command{
-		Name:  "install",
-		Usage: "Install a plugin from the catalog",
+		Name:      "install",
+		Usage:     "Install a plugin from the catalog",
+		Args:      true,
+		ArgsUsage: "<plugin-name>",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "plugin-dir",
+				Usage: "Directory where the plugin will be installed",
+				Value: defaultVaultPluginsDir,
+			},
+		},
 		Action: func(c *cli.Context) error {
 			// Implementation for installing a plugin goes here
 			// This could involve downloading the plugin binary from the remote URL,
