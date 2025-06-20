9c0f65c2e96709e7ebe6f43ea30355015c059b94
Socheat Sok <socheatsok78@gmail.com>
2025-06-20 11:04:13 +0700

    Minor refactor

--- pkg/catalog/release.go
+++ pkg/catalog/release.go
@@ -1,4 +1,4 @@
-package manager
+package catalog
 
 type Release struct {
 	PluginName    string
--- pkg/manager/manager_plugin_list.go
+++ pkg/manager/manager_plugin_list.go
@@ -1,12 +1,16 @@
 package manager
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/soramitsukhmer-lab/vault-plugin-catalog/pkg/catalog"
+)
 
 func (p *PluginManager) ListPlugins() error {
 	// This method should return a list of plugin names available in the catalog.
 	// Implementation will depend on how the catalog is structured (e.g., JSON, YAML).
 
-	catalog, err := NewCatalog(p.LocalCatalogPath)
+	catalog, err := catalog.NewCatalog(p.LocalCatalogPath)
 	if err != nil {
 		return fmt.Errorf("failed to load catalog: %w", err)
 	}
