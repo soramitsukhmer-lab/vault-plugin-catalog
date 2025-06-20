008bf891a0f218134a85e1037c6b8a348cecc4ac
Socheat Sok <socheatsok78@gmail.com>
2025-06-20 15:44:23 +0700

    Fix command failed to run due to missing directorires

--- command/commands.go
+++ command/commands.go
@@ -1,6 +1,10 @@
 package command
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/soramitsukhmer-lab/vault-plugin-catalog/pkg/pluginmanager"
 	"github.com/urfave/cli/v2"
 )
@@ -8,7 +12,7 @@ import (
 const (
 	defaultRemotePluginCatalogURL = "https://raw.githubusercontent.com/soramitsukhmer-lab/vault-plugin-catalog-database/refs/heads/main/catalog.json"
 	defaultLocalPluginCatalogPath = "/etc/vault-plugin-catalog/catalog.json"
-	defaultVaultPluginDir         = "/etc/vault/plugins"
+	defaultVaultPluginDir         = "/vault/plugins"
 )
 
 var pm = &pluginmanager.PluginManager{}
@@ -47,6 +51,35 @@ func Run(args []string) error {
 			uninstallPluginCommand(pm),
 			registerPluginCommand(pm),
 			updateCatalogCommand(pm),
+		},
+		Before: func(c *cli.Context) error {
+			localCatalogPath := c.String("local-catalog-path")
+			if localCatalogPath == "" {
+				return fmt.Errorf("local catalog path cannot be empty")
+			}
+
+			// Check if local catalog path is exist, if not, create it
+			strs := strings.Split(localCatalogPath, "/")
+			localCatalogDir := strings.Join(strs[:len(strs)-1], "/")
+			if _, err := os.Stat(localCatalogDir); os.IsNotExist(err) {
+				if err := os.MkdirAll(localCatalogDir, 0755); err != nil {
+					return fmt.Errorf("failed to create local catalog directory: %w", err)
+				}
+			}
+
+			vaultPluginDir := c.String("vault-plugin-dir")
+			if vaultPluginDir == "" {
+				return fmt.Errorf("vault plugin directory cannot be empty")
+			}
+
+			// Check if vault plugin directory exists, if not, create it
+			if _, err := os.Stat(vaultPluginDir); os.IsNotExist(err) {
+				if err := os.MkdirAll(vaultPluginDir, 0755); err != nil {
+					return fmt.Errorf("failed to create vault plugin directory: %w", err)
+				}
+			}
+
+			return nil
 		},
 	}
 
